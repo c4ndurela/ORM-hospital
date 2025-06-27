d3a96f8fe9fd60e76493ad19a90c5aa5c0e667ed
M. Candela Laffatigue <134436865+c4ndurela@users.noreply.github.com>
2025-06-27 11:33:29 -0300

    Update PatientRepository.java

--- src/main/java/com/ejemplo/hospital/repository/PatientRepository.java
+++ src/main/java/com/ejemplo/hospital/repository/PatientRepository.java
@@ -6,5 +6,17 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-    //  definir métodos personalizados 
+    // acá definir métodos personalizados 
+    // Buscar pacientes por nombre
+List<Patient> findByFirstName(String firstName);
+
+// Buscar pacientes por apellido que contengan una palabra
+List<Patient> findByLastNameContaining(String keyword);
+
+// Buscar pacientes por edad mayor a un valor dado
+List<Patient> findByAgeGreaterThan(int edad);
+
+// Buscar por si están aceptados o no
+List<Patient> findByAcceptedTrue(); // o findByAcceptedFalse()
+
 }
