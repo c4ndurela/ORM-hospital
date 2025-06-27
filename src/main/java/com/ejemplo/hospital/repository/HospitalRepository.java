287dea754f8455c89b0db9cd7594e1dc99f5a182
M. Candela Laffatigue <134436865+c4ndurela@users.noreply.github.com>
2025-06-27 15:44:42 -0300

    Create HospitalRepository.java

---
+++ src/main/java/com/ejemplo/hospital/repository/HospitalRepository.java
@@ -0,0 +1,10 @@
+package com.ejemplo.hospital.repository;
+
+import com.ejemplo.hospital.model.Hospital;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+    // ver
+}
